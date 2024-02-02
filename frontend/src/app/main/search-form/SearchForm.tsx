c39cf4383b34b6ae56a32a661d63e08eb1d443c0
Natalia Legarska <89125180+natalia-legarska@users.noreply.github.com>
2024-02-02 06:39:06 +0100

    reformat code

--- frontend/src/app/main/search-form/SearchForm.tsx
+++ frontend/src/app/main/search-form/SearchForm.tsx
@@ -1,12 +1,12 @@
 import {Autocomplete, Button, Stack, TextField} from "@mui/material";
-import {DatePicker, LocalizationProvider} from "@mui/x-date-pickers";
+import {LocalizationProvider} from "@mui/x-date-pickers";
 import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
 import {useState} from "react";
 
 const doiList = [
-    { label: '10.1016/j.chb.2021.106722', id: 1 },
-    { label: '10.1037/ppm0000219', id: 2 },
-    { label: '10.1145/3375629', id: 3 },
+    {label: '10.1016/j.chb.2021.106722', id: 1},
+    {label: '10.1037/ppm0000219', id: 2},
+    {label: '10.1145/3375629', id: 3},
 ];
 export const SearchForm = ({onFetch}: { onFetch: (doi: string) => void }) => {
     const [inputDoi, setInputDoi] = useState("");
@@ -16,13 +16,13 @@ export const SearchForm = ({onFetch}: { onFetch: (doi: string) => void }) => {
             <Stack spacing={2} direction="row">
                 {/*<DatePicker label="From"/>*/}
                 {/*<DatePicker label="To"/>*/}
-                <Autocomplete renderInput={(params) => <TextField {...params} label="DOI" />}
+                <Autocomplete renderInput={(params) => <TextField {...params} label="DOI"/>}
                               options={doiList.map(option => option.label)}
                               onChange={(event, newValue) => {
                                   setInputDoi(newValue || "");
                               }}
                               disablePortal
-                              sx={{ width: 300 }}
+                              sx={{width: 300}}
                               freeSolo
                 />
                 <Button onClick={() => onFetch(inputDoi)}
