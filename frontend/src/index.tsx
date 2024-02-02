81a9c30327d08b7c5765ef68f421f534728b8e9e
Natalia Legarska <89125180+natalia-legarska@users.noreply.github.com>
2024-02-02 03:36:04 +0100

    Add Apollo Client to the app

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './app/App';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import reportWebVitals from './reportWebVitals';
+import {ApolloProvider} from "@apollo/client";
+import {client} from "./svc/apiClient";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+      <ApolloProvider client={client}>
+          <App />
+      </ApolloProvider>
   </React.StrictMode>
 );
 
