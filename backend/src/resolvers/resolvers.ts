0d9ac903ba2fb6adf6c7ef31a3316be687ae50a2
Natalia Legarska <89125180+natalia-legarska@users.noreply.github.com>
2024-02-02 07:17:29 +0100

    Cleanup

--- backend/src/resolvers/resolvers.ts
+++
@@ -1,9 +0,0 @@
-import {ArticlesDataSource} from "../datasources/articles";
-
-export const resolvers = {
-    Query: {
-        articles: (parent, args, { dataSources }, info) => {
-            return dataSources.Article.getArticles();
-        }
-    },
-}
